3ff6c57f5f4da9607442433454cd26340f66c9fc
TeddiZen <2576903455@qq.com>
2026-05-25 23:22:50 +0800

    尝试重新自己写页面

--- Tlias-test/src/views/emp/index.vue
+++ Tlias-test/src/views/emp/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { getEmpList } from '@/api/emp'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 
 const emps = ref({
   code: null,
@@ -10,6 +10,25 @@ const emps = ref({
   msg: null
 })
 
+const formInline = ref({
+  name: '',
+  sex: '',
+  date: [],
+  begin: '',
+  end: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+
+watch(() => formInline.value.date, (newVal, oldVal) => {
+  if (newVal.length > 0) {
+    formInline.value.begin = newVal[0]
+    formInline.value.end = newVal[1]
+  }
+}, { deep: true })
+
 
 onMounted(async () => {
   emps.value = await getEmpList()
@@ -18,7 +37,42 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
+  <div id="empIndex">
+    <div>
+      <h2>员工管理</h2>
+    </div>
+    <span>
+      {{ formInline }}
+    </span>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="姓名">
+        <el-input v-model="formInline.name" placeholder="请输入姓名" clearable />
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-select
+          v-model="formInline.sex"
+          placeholder="请选择性别"
+          clearable
+        >
+          <el-option label="男" value="1" />
+          <el-option label="女" value="2" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="日期范围">
+        <el-date-picker
+          v-model="formInline.date"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          value-format="YYYY-MM-DD"
+          :default-time="defaultTime"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button type="info" @click="onSubmit">清空</el-button>
+      </el-form-item>
+    </el-form>
     <div v-for="emp in emps.data.rows" :key="emp.id">
       {{ emp.name }}
     </div>
